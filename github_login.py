61a585e2016c8f26a71a08c5a1275d7d4de28659
Ararat96 <ararat.hovhannisyan7@gmail.com>
2020-06-21 12:04:55 +0400

    github_login_selenium with wait

--- github_login.py
+++ github_login.py
@@ -1,8 +1,13 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome('Drivers/chromedriver.exe')
 driver.implicitly_wait(10)
+driver.maximize_window()
 driver.get('https://github.com/')
 assert driver.current_url == 'https://github.com/'
 sign_in = driver.find_element_by_link_text('Sign in')
@@ -13,5 +18,9 @@ password = driver.find_element_by_xpath('//input[@name="password"]')
 password.send_keys('Manukyan719')
 sign_in_button = driver.find_element_by_xpath('//input[@name = "commit"]')
 sign_in_button.click()
+try:
+    wait = WebDriverWait(driver, 10)
+    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'css-truncate-target')))
 
-#driver.quit()
+finally:
+    driver.quit()
